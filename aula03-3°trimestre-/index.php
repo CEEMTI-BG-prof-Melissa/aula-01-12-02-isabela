a161b74ee2f49c4db6e8d04a9b16a13279084b9e
isabelaponaht <isabela.nponaht2@aluno.edu.es.gov.br>
2025-09-09 11:09:16 -0300

    Create index.php

---
+++ aula03-3°trimestre-/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Aula 03 php</title>
+    </head>
+    <body>
+        <?php
+        $nome1="Gustavo";
+        $nome2="Isabela";
+
+        $n1=10;
+        $n2=2;
+        $soma= $n1=$n2;
+
+        echo "meu nome é" .$nome1 ."e minha dupla é" .$nome2;
+        echo "<br> meu nome é" .$nome1 ."e minha dupla é" .$nome2;
+
+        echo "<br> <br> A soma de $n1 com $n2 e igual $soma."
+        ?>
+    </body>
+</html>
