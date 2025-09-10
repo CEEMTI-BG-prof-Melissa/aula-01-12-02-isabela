f62a721e42807be64e7e2a27a40e81d714a31eae
isabelaponaht <isabela.nponaht2@aluno.edu.es.gov.br>
2025-09-10 10:55:13 -0300

    Update index.php

--- aula03-3°trimestre-/index.php
+++ aula03-3°trimestre-/index.php
@@ -9,12 +9,22 @@
 
         $n1=10;
         $n2=2;
-        $soma= $n1=$n2;
+        $soma= $n1+$n2;
 
         echo "meu nome é" .$nome1 ."e minha dupla é" .$nome2;
         echo "<br> meu nome é" .$nome1 ."e minha dupla é" .$nome2;
 
-        echo "<br> <br> A soma de $n1 com $n2 e igual $soma."
+        echo "<br> <br> A soma de $n1 com $n2 e igual $soma.";
+
+        echo "<br> <br> O resultadado da subtração de $n1 com $n2 é igual a:".($n1-$n2);
+
+        echo "<br> <br> O resultadado da multiplicação de $n1 com $n2 é igual a:".($n1*$n2);
+
+        echo "<br> <br> O resultadado da divisão real de $n1 com $n2 é igual a:".($n1/$n2);
+
+        echo "<br> <br> O resultadado do módulo de $n1 com $n2 é igual a:".($n1%$n2);
+
+
         ?>
     </body>
 </html>
